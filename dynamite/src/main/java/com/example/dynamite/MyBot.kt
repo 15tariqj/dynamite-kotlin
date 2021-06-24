f0c3818c6e32a615ba53fd9537bde73d8078b501
15tariqj <15tariqj@gmail.com>
2021-06-24 15:08:43 +0100

    keep track of opponents dynamite

--- dynamite/src/main/java/com/example/dynamite/MyBot.kt
+++ dynamite/src/main/java/com/example/dynamite/MyBot.kt
@@ -5,15 +5,27 @@ import com.softwire.dynamite.game.Gamestate
 import com.softwire.dynamite.game.Move
 
 class MyBot : Bot {
+    var myDynamite: Int = 100
+    var opponentsDynamite: Int = 100
     override fun makeMove(gamestate: Gamestate): Move {
         // Are you debugging?
         // Put a breakpoint in this method to see when we make a move
-        return Move.R
+        if (gamestate.rounds.isNotEmpty()){
+            if (gamestate.rounds.last().p2.equals(Move.W)) this.opponentsDynamite--
+            return Move.R
+        }
+        return Move.D
     }
 
     init {
         // Are you debugging?
         // Put a breakpoint on the line below to see when we start a new match
         println("Started new match")
+
+
     }
+
 }
+
+//Strategy:
+//
